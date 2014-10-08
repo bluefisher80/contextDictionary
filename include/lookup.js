fa8b3c1def94fe7235992dc8d50bf02f75641ab5
Jim Qiu <bluefisher80@gmail.com>
2014-10-08 23:06:00 +0800

    ported CSS and html from condic firefox version

--- include/lookup.js
+++ include/lookup.js
@@ -139,21 +139,35 @@ function event_mouseup(e) {
                 var hhitshop = doc.getElementsByTagName("dict");
                 for (var i = 0; i< hhitshop.length; i++){
                     shop =  hhitshop[i]; 
-                    WrHtml += '<p class="phonetic"><span> ';
+
+
+
+                    var key = shop.getElementsByTagName("key")[0].firstChild.nodeValue;
+                    key = escapeHTML(key);
+
+                    if(shop.getElementsByTagName("ps").length > 0 || shop.getElementsByTagName("pos").length > 0){
+                       // WrHtml += "<div id=key><strong>" + key + "</strong></div>";
+                    }else{
+                       //WrHtml += "<div id=key><strong>Sorry, no definition found for the word.</strong></div>";
+                    }
+
+
+                    WrHtml += '<div class="phonetic">';
 
                     for(var c = 0; c< shop.getElementsByTagName("ps").length; c++){
                         if(shop.getElementsByTagName("ps")[c].firstChild){
-                            WrHtml += '[' + escapeHTML(shop.getElementsByTagName("ps")[c].firstChild.nodeValue) + ']';
-                            WrHtml += "&nbsp;&nbsp;&nbsp;";
+                            WrHtml += '<div class=ps><strong>[' + escapeHTML(shop.getElementsByTagName("ps")[c].firstChild.nodeValue) + ']</strong></div>';
+                            WrHtml += '<audio controls><source src=' + escapeHTML(shop.getElementsByTagName("pron")[c].firstChild.nodeValue) +'></audio>';
+
                         }
                     }
 
-                    WrHtml += '</span></p>';
+                    WrHtml += '</div>';
 
                     for (var e = 0; e< shop.getElementsByTagName("pos").length; e++){
                         WrHtml += "<p>";
                         if(shop.getElementsByTagName("pos")[e].firstChild){
-                            WrHtml += escapeHTML(shop.getElementsByTagName("pos")[e].firstChild.nodeValue);
+                            WrHtml += "<strong>" + escapeHTML(shop.getElementsByTagName("pos")[e].firstChild.nodeValue) + "</strong>";
                             WrHtml += "&nbsp;&nbsp;&nbsp;";
                         }
                         WrHtml += escapeHTML(shop.getElementsByTagName("acceptation")[e].firstChild.nodeValue);
