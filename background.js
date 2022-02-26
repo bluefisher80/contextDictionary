e6aeec65b2a0659b1a9f2d33aab6af0385dfa663
jim <jim@mbp15.lan>
2022-02-26 21:32:25 +0800

    in the road of DictionaryAnywhere

--- background.js
+++ background.js
@@ -1,13 +1,93 @@
-var dic_url = "http://dict-co.iciba.com/api/dictionary.php?key=9A801B3C3A8D0AB5A5059C2F4B71AC50&w=";
-var bank_url = "http://www.context-dictionary.com/add/?word=" ;
+var dic_url = "https://dict-co.iciba.com/api/dictionary.php?key=9A801B3C3A8D0AB5A5059C2F4B71AC50&w=";
+var bank_url = "https://www.context-dictionary.com/add/?word=" ;
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+let DICMode_Google  =  true;
 
-    fetch(bank_url + message.selection + "&pageurl=" + message.theURL + "&context=" + message.theContext);
-    fetch(dic_url + message.selection.toLowerCase()).then(response => response.text())
-        .then(data => sendResponse(data));
+const GOOGLE_SPEECH_URI = 'https://www.google.com/speech-api/v1/synthesize',
+
+    DEFAULT_HISTORY_SETTING = {
+        enabled: true
+    };
+
+function extractMeaning (document, context) {
+    if (!document.querySelector("[data-dobid='hdw']")) { return null; }
+    
+    var word = document.querySelector("[data-dobid='hdw']").textContent,
+        definitionDiv = document.querySelector("div[data-dobid='dfn']"),
+        meaning = "";
+
+    if (definitionDiv) {
+        definitionDiv.querySelectorAll("span").forEach(function(span){
+            if(!span.querySelector("sup"))
+                 meaning = meaning + span.textContent;
+        });
+    }
+
+    meaning = meaning[0].toUpperCase() + meaning.substring(1);
+
+    var audio = document.querySelector("audio[jsname='QInZvb']"),
+        source = document.querySelector("audio[jsname='QInZvb'] source"),
+        audioSrc = source && source.getAttribute('src');
+
+    if (audioSrc) {
+        !audioSrc.includes("http") && (audioSrc = audioSrc.replace("//", "https://"));
+    }
+    else if (audio) {
+        let exactWord = word.replace(/·/g, ''), // We do not want syllable seperator to be present.
+            
+        queryString = new URLSearchParams({
+            text: exactWord, 
+            enc: 'mpeg', 
+            lang: context.lang, 
+            speed: '0.4', 
+            client: 'lr-language-tts', 
+            use_google_only_voices: 1
+        }).toString();
+
+        audioSrc = `${GOOGLE_SPEECH_URI}?${queryString}`;
+    }
+
+    return { word: word, meaning: meaning, audioSrc: audioSrc };
+};
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+//    fetch(bank_url + request.selection + "&pageurl=" + request.theURL +
+ //       "&context=" + request.theContext);
+
+    if(!DICMode_Google){
+        //Chinese dictioanry
+        fetch(dic_url + request.selection.toLowerCase()).
+                then(response => response.text())
+                .then(data => sendResponse(data));
 
     // return true from the event listener to indicate you wish to send a response asynchronously
     // (this will keep the message channel open to the other end until sendResponse is called).
+    //
     return true;
+
+
+    }else{
+
+
+   const { word, lang } = request, 
+        url = `https://www.google.com/search?hl=${lang}&q=define+${word}&gl=US`;
+    
+    fetch(url, { 
+            method: 'GET',
+            credentials: 'omit'
+        })
+        .then((response) => response.text())
+        .then((text) => {
+            const document = new DOMParser().parseFromString(text, 'text/html'),
+                content = extractMeaning(document, { word, lang });
+
+            sendResponse({ content });
+
+        })
+
+    return true;
+
+    }
 });
--- include/lookup.js
+++ include/lookup.js
@@ -1,3 +1,435 @@
+
+var DEFAULT_LANGUAGE = 'en',
+        DEFAULT_TRIGGER_KEY = 'none',
+
+        LANGUAGE,
+        TRIGGER_KEY;
+
+
+
+var longPressTimer, isLoading, loaded, isLongPressing, mouseDown, initEvent, selection, startOffset, rangeParentNode, theSelection,theURL,theContext;
+
+
+var cancelLongPress = function() {
+    clearTimeout(longPressTimer);
+    longPressTimer = null;
+}
+
+
+var onKikinGesture = function(event){
+//    console.log("bypassing the customDispatchEvent " + event.detail.status + " " + event.detail.reason );
+}
+
+
+window.addEventListener('mychromeGesture', onKikinGesture, true);
+var customDispatchEvent = function(s, r) {
+    window.dispatchEvent(new CustomEvent('mychromeGesture', {
+        //TODO
+        detail: {
+            status: s,
+            reason: r,
+            target: initEvent.target
+        }
+    }));
+}
+
+
+var onMouseUp = function(e) {
+    if (longPressTimer) {
+        isLongPressing = false;
+        cancelLongPress();
+        customDispatchEvent('failed', 'short');
+    }
+    mouseDown = false;
+    initEvent = null;
+
+    startOffset = 0;
+    rangeParentNode = null;
+    console.log("The initEvent object was cleared to null in the onMouseUp registered handler?" );
+};
+
+var onMouseMove = function(e) {
+    if (longPressTimer) {
+        isLongPressing = false;
+        cancelLongPress();
+        customDispatchEvent('failed', 'move');
+    }
+}
+
+var onScroll = function(e) {
+    if (longPressTimer) {
+        isLongPressing = false;
+        cancelLongPress();
+        customDispatchEvent('failed', 'scroll');
+    }
+}
+
+var onLongPress = function(e) {
+    console.log("Enter Long Pressing Detecting Mode");
+    // check that there is no selected text
+    var current = window.getSelection().toString();
+    if (selection != current && current) {
+        console.log("Game over");
+        return};
+    
+    // update status
+    isLongPressing = true;
+    longPressTimer = null;
+
+    console.log("Start to call the lookup communicate method");
+    //lookupWord();    
+
+    showMeaning(initEvent);
+ 
+};
+
+
+var startLoading = function(clbk) {
+    // only load the thing once
+    if (isLoading) return;
+    isLoading = true;
+    console.log("loading here");	
+}
+
+
+function onClick(e){
+
+    var isLink = e.target.tagName == 'A' || (e.target.parentNode && e.target.parentNode.tagName == 'A');
+
+    if(isLink && isLongPressing){
+        e.preventDefault();
+    }
+
+
+}
+
+
+function onMouseDown(e) {
+
+    console.log("this is inside the mousedown check");
+    // check if left click
+    if (e.which != 1) return;
+    
+    // check if it's in an input
+    if (/^(INPUT|TEXTAREA|SELECT|HTML)$/.exec(e.target.tagName)) return;
+    
+    // save infos
+    isLongPressing = false;
+    mouseDown = true;
+    //There is no window object in the this script TODO 
+    //selection = window.getSelection().toString();
+    selection = "TODO"
+    initEvent = e;
+    startOffset = initEvent.rangeOffset;
+    rangeParentNode = initEvent.rangeParent;
+       
+    console.log("The rangeParent of the event model " + initEvent.rangeParent);
+
+    console.log("Support document caretRangeFromPoint" + document.caretRangeFromPoint());
+    if(document.caretRangeFromPoint){
+       console.log("Support document caretRangeFromPoint" + document.caretRangeFromPoint());
+       range = document.caretRangeFromPoint(e.clientX, e.clientY);
+       console.log("The caretRangeFromPoint height " + range.getBoundingClientRect().height);
+       console.log("The caretRangeFromPoint left" + range.getBoundingClientRect().left);
+       console.log("The caretRangeFromPoint width" + range.getBoundingClientRect().width);
+       console.log("The caretRangeFromPoint top" + range.getBoundingClientRect().top);
+       console.log("The original event position X is " +  e.clientX + " Y is " +  e.clientY);
+       console.log("The range clicked by original event is " + range );
+       rangeParentNode = range.startContainer;
+       startOffset = range.startOffset; 
+       console.log("The rangeParentNode found within the caretRangeFromPoint method is " + rangeParentNode );
+    }
+
+    if(document.caretPositionFromPoint){
+        //firefox support
+        //
+        //https://developer.mozilla.org/en-US/docs/Web/API/Document/caretPositionFromPoint
+        //      range = document.caretPositionFromPoint(e.clientX, e.clientY);
+    //          textNode = range.offsetNode;
+    //          offset = range.offset;
+    }
+
+    theSelection = findTargetWord(startOffset, rangeParentNode);
+    theURL = window.document.URL;
+    theContext = rangeParentNode.textContent;
+    console.log("In the onMouseDown handler show the original event range offset " + startOffset);
+    console.log("In the onMouseDown handler show the window URL:" + window.document.URL);
+    console.log("In the onMouseDown handler show the original event content " + rangeParentNode.textContent);
+    
+    // launch a timer to detect "long press"
+    var isLink = e.target.tagName == 'A' || (e.target.parentNode && e.target.parentNode.tagName == 'A');
+    longPressTimer = setTimeout(onLongPress, isLink ? 2000: 700);
+};
+
+
+function findWordByEvent(event){
+       range = document.caretRangeFromPoint(event.clientX, event.clientY);
+       rangeParentNode = range.startContainer;
+       startOffset = range.startOffset; 
+
+       return findTargetWord(startOffset, rangeParentNode);
+
+}
+
+function lookupWord(){
+    //self.port.emit("lookup",theSelection,window.navigator.language);
+    handle_longpressing(initEvent);
+    //self.port.emit("lookup",theSelection,'ja');
+}
+
+function findTargetWord(startOffset, parentNode){
+
+    console.log("Enter find target word logic");
+    table = [];
+    
+    console.log("Enter find target word logic 1");
+    //disalbe this method for just right now
+    console.log("Enter find target word logic 1.1");
+
+    offset = startOffset;
+    console.log("Enter find target word logic 1.2");
+    
+    console.log("Enter find target word logic 2");
+    console.log("Enter find target node "+  parentNode.textContent);
+    var textarray = parentNode.textContent.split("");
+    var textlen = parentNode.textContent.length;
+
+
+    var forward = offset + 1;
+    var backword = offset- 1;
+
+    if(! /[a-zA-Z]/.test(textarray[offset])){
+        forward = textlen+1;
+        //the clicked char is not a normal char, so there is no need to continue forward-way.
+    }else{
+        //A letter belongs to [a-zA-Z]
+        table.splice(0,0,textarray[offset]);
+    }
+
+    console.log("Enter find target word logic 3");
+    while(backword>=0){
+        if(! /[a-zA-Z]/.test(textarray[backword])){break;}
+        else{
+            table.splice(0,0,textarray[backword]);
+        }
+        backword -=1;
+    }
+
+    console.log("Enter find target word logic 4");
+    while(forward <= textlen){
+        if(! /[a-zA-Z]/.test(textarray[forward])){break;}
+        else{
+            table.push(textarray[forward]);
+        }
+        forward +=1;
+    }
+
+    var word = table.join("");
+    console.log("Ready to print out the word");
+    console.log(word);
+    
+    return word;
+
+
+
+}
+
+
+  //the LongClick handler
+  window.addEventListener('mouseup', onMouseUp, true);
+  window.addEventListener('click', onClick, true);
+  window.addEventListener('mousedown', onMouseDown, true);
+  window.addEventListener('mousemove', onMouseMove, true);
+  window.addEventListener('scroll', onScroll, true);
+
+
+
+
+
+
+function showMeaning (event){
+    var createdDiv,
+        info = getSelectionInfo(event);
+
+    info.word = findWordByEvent(event)
+
+    if (!info) { return; }
+    retrieveMeaning(info)
+        .then((response) => {                
+            if (!response.content) { return noMeaningFound(createdDiv); }
+            appendToDiv(createdDiv, response.content);
+        });
+
+    // Creating this div while we are fetching meaning to make extension more fast.
+    createdDiv = createDiv(info);
+}
+
+
+function getSelectionInfo(event) {
+    var word;
+    var boundingRect;
+
+    boundingRect = getSelectionCoords(event);
+
+    var top = boundingRect.top + window.scrollY,
+        bottom = boundingRect.bottom + window.scrollY,
+        left = boundingRect.left + window.scrollX;
+
+    if (boundingRect.height == 0) {
+        top = event.pageY;
+        bottom = event.pageY;
+        left = event.pageX;
+    }
+
+    return {
+        top: top,
+        bottom: bottom,
+        left: left,
+        word: "template",
+        clientY: event.clientY,
+        height: boundingRect.height
+    };
+}
+
+function retrieveMeaning(info){
+    //return browser.runtime.sendMessage({ word: info.word, 
+    return chrome.runtime.sendMessage({ word: info.word, 
+                                         lang: LANGUAGE, 
+                                         time: Date.now() });
+}
+
+function createDiv(info) {
+    var hostDiv = document.createElement("div");
+
+    hostDiv.className = "dictionaryDiv";
+    hostDiv.style.left = info.left -10 + "px";
+    hostDiv.style.position = "absolute";
+    hostDiv.style.zIndex = "1000000"
+    hostDiv.attachShadow({mode: 'open'});
+
+    var shadow = hostDiv.shadowRoot;
+    var style = document.createElement("style");
+    //style.textContent = "*{ all: initial}";
+    style.textContent = ".mwe-popups{background:#fff;position:absolute;z-index:110;-webkit-box-shadow:0 30px 90px -20px rgba(0,0,0,0.3),0 0 1px #a2a9b1;box-shadow:0 30px 90px -20px rgba(0,0,0,0.3),0 0 1px #a2a9b1;padding:0;font-size:14px;min-width:300px;border-radius:2px}.mwe-popups.mwe-popups-is-not-tall{width:320px}.mwe-popups .mwe-popups-container{color:#222;margin-top:-9px;padding-top:9px;text-decoration:none}.mwe-popups.mwe-popups-is-not-tall .mwe-popups-extract{min-height:40px;max-height:140px;overflow:hidden;margin-bottom:47px;padding-bottom:0}.mwe-popups .mwe-popups-extract{margin:16px;display:block;color:#222;text-decoration:none;position:relative} .mwe-popups.flipped_y:before{content:'';position:absolute;border:8px solid transparent;border-bottom:0;border-top: 8px solid #a2a9b1;bottom:-8px;left:10px}.mwe-popups.flipped_y:after{content:'';position:absolute;border:11px solid transparent;border-bottom:0;border-top:11px solid #fff;bottom:-7px;left:7px} .mwe-popups.mwe-popups-no-image-tri:before{content:'';position:absolute;border:8px solid transparent;border-top:0;border-bottom: 8px solid #a2a9b1;top:-8px;left:10px}.mwe-popups.mwe-popups-no-image-tri:after{content:'';position:absolute;border:11px solid transparent;border-top:0;border-bottom:11px solid #fff;top:-7px;left:7px} .audio{background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAcUlEQVQ4y2P4//8/AyUYQhAH3gNxA7IAIQPmo/H3g/QA8XkgFiBkwHyoYnRQABVfj88AmGZcTuuHyjlgMwBZM7IE3NlQGhQe65EN+I8Dw8MLGgYoFpFqADK/YUAMwOsFigORatFIlYRElaRMWmaiBAMAp0n+3U0kqkAAAAAASUVORK5CYII=);background-position: center;background-repeat: no-repeat;cursor:pointer;margin-left: 8px;opacity: 0.5; width: 16px; display: inline-block;} .audio:hover {opacity: 1;}";
+    shadow.appendChild(style);
+
+    var encapsulateDiv = document.createElement("div");
+    encapsulateDiv.style = "all: initial; text-shadow: transparent 0px 0px 0px, rgba(0,0,0,1) 0px 0px 0px !important;";
+    shadow.appendChild(encapsulateDiv);
+
+
+    var popupDiv = document.createElement("div");
+    popupDiv.style = "font-family: arial,sans-serif; border-radius: 12px; border: 1px solid #a2a9b1; box-shadow: 0 0 17px rgba(0,0,0,0.5)";
+    encapsulateDiv.appendChild(popupDiv);
+
+
+    var contentContainer = document.createElement("div");
+    contentContainer.className = "mwe-popups-container";
+    popupDiv.appendChild(contentContainer);
+
+
+
+    var content = document.createElement("div");
+    content.className = "mwe-popups-extract";
+    content.style = "line-height: 1.4; margin-top: 0px; margin-bottom: 11px; max-height: none";
+    contentContainer.appendChild(content);
+
+
+    var heading = document.createElement("h3");
+    heading.style = "margin-block-end: 0px; display:inline-block;";
+    heading.textContent = "Searching";
+
+    var meaning = document.createElement("p");
+    meaning.style = "margin-top: 10px";
+    meaning.textContent = "Please Wait...";
+
+    var audio = document.createElement("div");
+    audio.className = "audio";
+    audio.innerHTML = "&nbsp;";
+    audio.style.display = "none";
+
+    var moreInfo =document.createElement("a");
+    moreInfo.href = `https://www.google.com/search?hl=${LANGUAGE}&q=define+${info.word}`;
+    moreInfo.style = "float: right; text-decoration: none;"
+    moreInfo.target = "_blank";
+
+    content.appendChild(heading);
+    content.appendChild(audio);
+    content.appendChild(meaning);
+    content.appendChild(moreInfo);
+    document.body.appendChild(hostDiv);
+
+    if(info.clientY < window.innerHeight/2){
+        popupDiv.className = "mwe-popups mwe-popups-no-image-tri mwe-popups-is-not-tall";
+        hostDiv.style.top = info.bottom + 10 + "px";
+        if(info.height == 0){
+            hostDiv.style.top = parseInt(hostDiv.style.top) + 8 + "px";
+        }
+    } else {
+        popupDiv.className = "mwe-popups flipped_y mwe-popups-is-not-tall";
+        hostDiv.style.top = info.top - 10 - popupDiv.clientHeight + "px";
+
+        if(info.height == 0){
+            hostDiv.style.top = parseInt(hostDiv.style.top) - 8 + "px";
+        }
+    }
+
+    return { 
+        heading, 
+        meaning, 
+        moreInfo, 
+        audio 
+    };
+
+}
+
+function getSelectionCoords(event) {
+    range = document.caretRangeFromPoint(event.clientX, event.clientY);
+    var oRect = range.getBoundingClientRect();
+    return oRect;
+}
+
+function appendToDiv(createdDiv, content){
+    var hostDiv = createdDiv.heading.getRootNode().host;
+    var popupDiv = createdDiv.heading.getRootNode().querySelectorAll("div")[1];
+
+    var heightBefore = popupDiv.clientHeight;
+    createdDiv.heading.textContent = content.word;
+    createdDiv.meaning.textContent = content.meaning;
+    createdDiv.moreInfo.textContent = "More »";
+
+    var heightAfter = popupDiv.clientHeight;
+    var difference = heightAfter - heightBefore;
+
+
+    if(popupDiv.classList.contains("flipped_y")){
+        hostDiv.style.top = parseInt(hostDiv.style.top) - difference + 1 + "px";
+    }
+
+    if(content.audioSrc){
+      var sound = document.createElement("audio");
+      sound.src = content.audioSrc;
+      createdDiv.audio.style.display  = "inline-block";
+      createdDiv.audio.addEventListener("click", function(){
+        sound.play();
+      });
+    }
+}
+
+function noMeaningFound (createdDiv){
+  createdDiv.heading.textContent = "Sorry";
+  createdDiv.meaning.textContent = "No definition found.";
+}
+
+function removeMeaning(event){
+    var element = event.target;
+    if(!element.classList.contains("dictionaryDiv")){
+        document.querySelectorAll(".dictionaryDiv").forEach(function(Node){
+            Node.remove();
+        });
+    }
+}
+
 function is_chinese(word) {
     return (/^[\u4e00-\u9fa5]+$/g).test(word);
 }
@@ -27,12 +459,10 @@ var haloword_html = '<div id="haloword-lookup" class="ui-widget-content" draggab
 </div>\
 <div id="haloword-control-container">\
  <!-- <a herf="#" id="haloword-close" class="haloword-button" title="关闭查询窗"></a> -->\
- <span><a href="http://www.context-dictionary.com/list/" target="_blank" title="查询历史">history</a></span>\
+ <span><a href="https://www.context-dictionary.com/list/" target="_blank" title="查询历史">history</a></span>\
 </div>\
 <br style="clear: both;" />\
 </div><div id="haloword-content"></div></div>';
-
-//$("body").append(haloword_html);
 
 var div = document.createElement('div');
 div.className = 'tooltip';
@@ -62,14 +492,14 @@ document.addEventListener("DOMNodeInserted", function(event) {
     }
 });
 
-$("body").click(event_click);
+document.addEventListener('click', event_click);
 
 function event_click(event) {
     console.log("this method was triggered in the event_click method");
     console.log("In event_click, the type of the event is " + event.type);
     if (haloword_opened && !isLongPressing) {
-        var target = $(event.target);
-        if (target.attr("id") != "haloword-lookup" && !target.parents("#haloword-lookup")[0]) {
+        var target = event.target;
+        if (target.id != "haloword-lookup" && !target.parents("#haloword-lookup")[0]) {
             document.getElementById("haloword-lookup").style.display = "none";
             haloword_opened = false;
         }
@@ -110,7 +540,10 @@ function handle_longpressing(event) {
 
         var result;
 
-        chrome.runtime.sendMessage({selection: theSelection,theURL: theURL , theContext: theContext}, (response) => {
+        chrome.runtime.sendMessage({selection: theSelection,
+                                    theURL: theURL ,
+                                    theContext: theContext}, 
+            (response) => {
             console.log('received user data in promise action, prepare to parse for the data and show it' );
             document.getElementById("haloword-content").innerHTML = parseDicData(response);
 
@@ -124,7 +557,7 @@ function handle_longpressing(event) {
         document.getElementById("haloword-lookup").style.display = "block";
 
         document.getElementById("haloword-content").innerHTML = "<p>Loading definitions...</p>"
-        console.log("handle_longpressing method,the #halowword-lookup div is shown.  ");
+        console.log("the #halowword-lookup div is shown.  ");
 
         // HACK: fix dict window not openable
 
@@ -182,3 +615,16 @@ function parseDicData(data){
 
     return WrHtml;
 }
+
+
+    (function () {
+        //let storageItem = browser.storage.local.get();
+        let storageItem = chrome.storage.local.get();
+
+        storageItem.then((results) => {
+            let interaction = results.interaction || { dblClick: { key: DEFAULT_TRIGGER_KEY }};
+
+            LANGUAGE = results.language || DEFAULT_LANGUAGE;
+            TRIGGER_KEY = interaction.dblClick.key;
+        });
+    })();
